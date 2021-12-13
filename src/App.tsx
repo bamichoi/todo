3ac5dbc1d0329ed9e8fb0baa1d1c7526965c3541
beomseok.choi <cbs412@naver.com>
2021-12-13 14:54:44 +0900

    react-hook-form magik 1/3 of challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
+import ToDoList from "ToDoList";
 
 
 
@@ -87,6 +88,7 @@ function App() {
   return ( 
     <>
         <GlobalStyle />
+        <ToDoList />
     </>
   )
 }

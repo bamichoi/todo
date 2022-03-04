f6ca46236ed9c018d381954a804904d5279f5ab4
beomseok.choi <cbs412@naver.com>
2022-03-04 12:53:43 +0900

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# To Do React.js
+#  ToDo Arancia
 
-To Do app for React.js Challenge of Nomad coder using React.js, TS and Recoil
+React와 Recoil을 이용한 간단하고 귀여운 ToDo App🍊
+So cute ToDo app usig React and Recoil
+
+https://bamichoi.github.io/todo/
+
+## 💻개발 언어 및 환경 Languages
+
+Frontend : React.js, TypeScript
+
+## ⏱개발 기간 Development period
+  
+2021년 12월 8일 ~ 2021년 12월 15일 
+December 8, 2021 ~ December 15, 2021  
+  
+
+## 📝 개발 후기 Developer Review
+
+
+### 아이디어 Idea
+
+- 노마드 코더 리액트 챌린지의 과제 중 하나였다. Recoil을 이용해서 진행 상태별로 목록을 나눈 toDo 앱을 만드는 거였는데 뭔가 엄청 귀여운 ToDo를 만들고 싶어졌다.
+
+
+### 개발 과정에서의 문제 Problems & solution in development
+
+- Recoil을 연습하면서 state의 불변성의 개념과 그 이유를 이해하는데 많이 어려움을 겪었다. state가 mutate되면 변경 내역들을 추적할 수 없다. 만약 이전 state 자체를 변경한다면 참조 값이 동일하게 되고 리액트는 이전 상태와 현재 상태를 비교할 수 없을 것이다. 또한 참조만 비교하게 되면 값 하나씩을 비교하는 것보다 변경을 감지하는데에 훨씬 효율적이다. 
+
+
+### 이 프로젝트를 통해 배운 것 So What I learn
+
+- prop을 통해 직접 상태값을 전달할때 나타날 수 있는 비효율성과 Recoil이 어떤 방식으로 이 문제를 해결하는지 알아보았다.
+  
+- state의 immutability에 대한 이해.
+- 
+- atom과 selector의 개념을 이해하고 활용.
+
+- Recoil을 사용한 어플리케이션 상태관리.
 
--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -1,4 +1,10 @@
-import { Categories, categoryState, customCategoryState, toDoSelector, toDoState } from "atom";
+import {
+  Categories,
+  categoryState,
+  customCategoryState,
+  toDoSelector,
+  toDoState,
+} from "atom";
 import React, { useEffect, useState } from "react";
 import CreateToDo from "./CreateToDo";
 import { selector, useRecoilState, useRecoilValue } from "recoil";
@@ -7,195 +13,105 @@ import CreateCategory from "./CreateCategory";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    display:flex;
-    flex-direction:column;
-    align-items: center; 
-    height:100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
 `;
 
 const Title = styled.div`
-    font-size : 30px;
-    margin-top: 30px;
-    margin-bottom :30px;
-    font-weight:500;
-    
+  font-size: 30px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  font-weight: 500;
 `;
 
 const CategoryDiv = styled.div`
-    display:flex;
-    justify-content:space-between;
-    align-self: center;
-    margin-bottom : 30px;
-    
-    div {
-        font-size:20px;
-        height:30px;
-        display:flex;
-        align-items:center;
-    }
-    select {
-        height: 30px;
-        width: 70px;
-        font-family: 'Gaegu', cursive;
-        border:none;
-        border-radius:5px;
-        margin-right: 10px;
-        option {
-         text-align:center;
-        }
-       
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+  margin-bottom: 30px;
 
+  div {
+    font-size: 20px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+  }
+  select {
+    height: 30px;
+    width: 70px;
+    font-family: "Gaegu", cursive;
+    border: none;
+    border-radius: 5px;
+    margin-right: 10px;
+    option {
+      text-align: center;
     }
+  }
 `;
 
 const CategoryTitle = styled.h1`
-    margin-bottom: 30px;
-    font-size:20px;
-
+  margin-bottom: 30px;
+  font-size: 20px;
 `;
 
 const Ul = styled.ul`
-    height: 100%;
-    margin-bottom: 50px;
+  height: 100%;
+  margin-bottom: 50px;
 `;
 const Footer = styled.div`
-    text-align : center;
-    position:fixed;
-    bottom:0;
-    width:100%;
-    padding:5px;
-    background-color: ${props => props.theme.bgColor};
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 5px;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 function ToDoList() {
-    const allToDos = useRecoilValue(toDoState)
-    const toDos = useRecoilValue(toDoSelector);
-    const customCategories = useRecoilValue(customCategoryState)
-    const [ category, setCategory ] = useRecoilState(categoryState);
-    const onInput = (event:React.FormEvent<HTMLSelectElement>) => {
-        const { currentTarget : { value }} = event;
-        setCategory(value as any);
-    }
-    useEffect(() => {
-        localStorage.setItem("TODOS", JSON.stringify(allToDos))
-    }, [allToDos])
-    return (
-        <>
-        <Wrapper>
-            <Title>오늘의 할 일🍊</Title>
-            <CategoryDiv>
-                <div>분류:&nbsp;</div>
-                <select value={category} onInput={onInput}>
-                    <option value={Categories.TO_DO}>할 일</option>
-                    <option value={Categories.DOING}>진행 중</option>
-                    <option value={Categories.DONE}>끝낸 일</option>
-                    {customCategories.map(category => (
-                        <option key={category.id} value={category.title}>{category.title}</option>
-                    ))}
-                </select>
-                <CreateCategory />
-            </CategoryDiv>
-            <CreateToDo />
-            <CategoryTitle>{category}</CategoryTitle>
-            <Ul>
-                {toDos.map(toDo => (
-                    <ToDo key={toDo.id} {...toDo}/>
-                    ))}
-            </Ul>
-    </Wrapper>
-    <Footer>
-    &copy; 2021 To Do Arancia All rights reserved.
-    </Footer>
-    </> 
-    )  
+  const allToDos = useRecoilValue(toDoState);
+  const toDos = useRecoilValue(toDoSelector);
+  const customCategories = useRecoilValue(customCategoryState);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setCategory(value as any);
+  };
+  useEffect(() => {
+    localStorage.setItem("TODOS", JSON.stringify(allToDos));
+  }, [allToDos]);
+  return (
+    <>
+      <Wrapper>
+        <Title>오늘의 할 일🍊</Title>
+        <CategoryDiv>
+          <div>분류:&nbsp;</div>
+          <select value={category} onInput={onInput}>
+            <option value={Categories.TO_DO}>할 일</option>
+            <option value={Categories.DOING}>진행 중</option>
+            <option value={Categories.DONE}>끝낸 일</option>
+            {customCategories.map((category) => (
+              <option key={category.id} value={category.title}>
+                {category.title}
+              </option>
+            ))}
+          </select>
+          <CreateCategory />
+        </CategoryDiv>
+        <CreateToDo />
+        <CategoryTitle>{category}</CategoryTitle>
+        <Ul>
+          {toDos.map((toDo) => (
+            <ToDo key={toDo.id} {...toDo} />
+          ))}
+        </Ul>
+      </Wrapper>
+      <Footer>&copy; 2021 To Do Arancia by Bami All rights reserved.</Footer>
+    </>
+  );
 }
 
-export default ToDoList
-
-
-// witout react-hook-form
-/* 
-function ToDoList() {
-    const [ toDo, setToDo ] = useState("");
-    const [ todoError, setToDoError ] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget : { value } } = event;
-        setToDo(value);
-        setToDoError("");   
-    }
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if(toDo.length < 10){
-            setToDoError("To do should be longer")
-        }
-        console.log("submit");
-    }
-    return <div>
-        <form onSubmit={onSubmit}>
-            <input onChange={onChange} value={toDo} placeholder="Write to do" />
-            <button>Add</button>
-            {todoError !== "" ? todoError : null }
-        </form>
-    </div>
-} */
-
-
-// with react-hook-form!
-/* 
-interface IForm {
-    toDo: string;
-    password: string;
-    password2: string;
-    extraError?: string;
-}
-
-function ToDoList() {
-    const { register, handleSubmit, 
-        formState: { errors }, setError 
-    } = useForm<IForm>({
-        defaultValues: {
-            toDo: "Happy Coding"
-        }
-    });
-    const onValid = (data: IForm) => {
-        if (data.password !== data.password2) {
-            setError("password", { message : "Passwords does not match"}, {shouldFocus:true})
-        }
-        //setError("extraError", { message : "server offline"})
-    }
-    console.log(errors)
-    return <div style={{display:"flex", justifyContent:"center",}}>
-        <form onSubmit={handleSubmit(onValid)}>
-            <span>
-                {errors?.toDo?.message}
-            </span>
-            <input {...register("toDo", { 
-                required : "To do is required",
-                validate : (value)=> value.includes("pain") ? "no pain allowed" : true,
-                pattern : {
-                    value:/^[A-Za-z0-9._%+-]+@naver.com$/,
-                    message: "only naver.com email allowed"
-                },
-                minLength: {
-                value:5, 
-                message:"value is too short!"
-                } 
-                })} 
-                placeholder="Write to do" />
-            <span>
-                {errors?.password?.message}
-            </span>
-            <input {...register("password", { 
-                required : "password is required",
-                minLength : 5 
-            },    
-            )} placeholder="passowrd" />
-            <input {...register("password2", { 
-                required : "password2 is required",
-                minLength : 5 
-            },    
-            )} placeholder="passowrd2" />
-            <button>Add</button>
-        </form>
-    </div>
-} */
+export default ToDoList;
